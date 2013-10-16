d508652072c6cb797bb2f2f3fe6ec56d50ab15b1
n3138 <n3138@ndensan.co.jp>
2013-10-16 16:55:29 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyCloud::Application.config.secret_key_base = 'bac2a7de9e3676f88f11cb84a46e7adc131fb6af461bb65d1a00f994021906f8edeb6f2ef9034d7cf175cf97423cf209d97ef955869722b0d3369c20050df1c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyCloud::Application.config.session_store :cookie_store, key: '_myCloud_session'
