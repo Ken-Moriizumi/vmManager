2fcb94340dbca9744f05c515da1469778fc9f199
n3138 <n3138@ndensan.co.jp>
2013-10-18 09:37:23 +0900

    macaddr取得 isTemplate実装

--- app/models/myvminfo.rb
+++ app/models/myvminfo.rb
@@ -1,6 +1,6 @@
 class Myvminfo
 
-  attr_reader :vm, :name,:path,:ipaddress, :powerstatus
+  attr_reader :vm, :name,:path,:ipaddress, :powerstatus ,:macaddress
 
   def initialize(vm,path)
       @vm = vm
@@ -8,6 +8,17 @@ class Myvminfo
       @path = path
       @ipaddress = vm.guest.ipAddress
       @powerstatus = vm.runtime.powerState
+      @macaddress = get_macaddr_from_vm
+  end
+  
+  def isTemplate?
+      @vm.config.template
   end
 
+private
+  def get_macaddr_from_vm
+      @vm.config.hardware.device.each do |dev|
+           return dev.macAddress if dev.deviceInfo.label == "ネットワーク アダプタ 1"
+      end
+  end
 end
--- spec/models/myvminfo_spec.rb
+++ spec/models/myvminfo_spec.rb
@@ -5,10 +5,12 @@ describe Myvminfo do
        before do
           vim = RbVmomi::VIM.connect host: '172.17.50.61', user: 'Administrator', password: '10Katu'
           @dc ||= vim.serviceInstance.find_datacenter("ZR") or fail "datacenter not found"
-          @myvm = Myvminfo.new(@dc.find_vm("/開発用/業務/devServerXX01"),"/開発用/業務/devServerXX01")
        end
 
-       context "メンバの取得" do
+       context "VMメンバの取得" do
+          before do
+             @myvm = Myvminfo.new(@dc.find_vm("/開発用/業務/devServerXX01"),"/開発用/業務/devServerXX01")
+          end
           it "vm is VirtualMachine" do
              @myvm.vm.class.should eq RbVmomi::VIM::VirtualMachine
           end
@@ -23,6 +25,20 @@ describe Myvminfo do
           end
           it "電源状態 is poweredOn" do
              @myvm.powerstatus.should eq "poweredOn"
+          end
+          it "is テンプレート？" do
+             @myvm.isTemplate?.should eq false
+          end
+          it "MACアドレス is hoge" do
+             @myvm.macaddress.should eq "00:50:56:9b:4d:ce"
+          end
+       end 
+       context "テンプレートメンバの取得" do
+          before do
+             @myvmT = Myvminfo.new(@dc.find_vm("/開発用/業務/devServerXX_template"),"/開発用/業務/devServerXX_template")
+          end
+          it "is テンプレート？" do
+             @myvmT.isTemplate?.should eq true
           end
        end 
     end
