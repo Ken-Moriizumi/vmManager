62c2c1dfd12976a06bb24e5e4c88d3b6c163bd88
n3138 <n3138@ndensan.co.jp>
2013-11-05 14:47:41 +0900

    ネットワーク名とパスはpopoverで表示

--- app/helpers/vminfos_helper.rb
+++ app/helpers/vminfos_helper.rb
@@ -1,2 +1,24 @@
 module VminfosHelper
+   def insert_popup_script()
+     javascript_tag( "$(function () { $('[rel=popover]').popover({html: true}); });")
+   end
+
+   def vm_popup_content(vminfo)
+     content = content_tag(:a,create_vm_popup_hash(vminfo),:escape => false) do 
+       vminfo.name
+     end
+     content.gsub(/改行/," \<br\>")
+   end
+
+   def create_vm_popup_hash(vminfo)
+     {
+        "rel" => "popover",
+        "data-title" => vminfo.name,
+        "data-html" => "true",
+        "data-content" => "
+           Path : #{vminfo.path}#{"改行"}
+           NetWork : #{vminfo.network}
+        "
+     }
+   end
 end
